05c891c195d5a23978bc967de2b28f403c04379b
caleb <caleb@syndicate.io>
2023-08-25 23:31:27 -0400

    up

--- apps/caleb.ooo/app/page.tsx
+++ apps/caleb.ooo/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@ui/components/button";
 export default function Page() {
   return (
     <>
-      <h1>Web</h1>
+      <h1>offline</h1>
       <Button>Click me</Button>
     </>
   );
