3169003695e93aeec7e26c5256db3efcbaed5444
caleb <calebcarithers@me.com>
2023-10-02 16:41:08 -0500

    rm

--- apps/caleb.online/app/api/test/number/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server"
-
-export function GET() {
-  const value = 3.14
-  return NextResponse.json(value)
-}
--- apps/caleb.online/app/api/test/string/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server"
-
-export function GET() {
-  const value = "Hello world"
-  return NextResponse.json(value)
-}
